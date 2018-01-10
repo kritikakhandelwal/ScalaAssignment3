e55a974e29b9e704794c03aa057b6936290629e5
Kritikakhandelwal <kritika.khandelwal@knoldus.in>
2018-01-10 21:57:08 +0530

    FibonacciQuestionAdded

--- src/main/scala/Assignment3/Operations.scala
+++ src/main/scala/Assignment3/Operations.scala
@@ -2,7 +2,7 @@ package Assignment3
 
 class Operations {
   def sumOfLists(list1: List[Int],list2: List[Int]): List[Int] = {
-if(list1.length == list2.length) {
+
   val sum = for (i <- 0 until list1.length)
     yield list1(i) + list2(i)
 
@@ -12,7 +12,7 @@ if(list1.length == list2.length) {
 }
 
 
-  }
+
   def doubleElements(list: List[Int]): List[Int] = {
 
     list.map(x => x + x)
@@ -34,6 +34,13 @@ if(list1.length == list2.length) {
 
   }
   */
+  /*def fibonacci(first:Int,second:Int,n:Int):List[Int] = {
+    n match {
+      case 0 => 1
+      case 1 => 2
+      case _ => fibonacci(second,first + second,n-1)
+    }
+  }*/
 
   def listLength(list1:List[Int]): Int = {
     list1 match {
@@ -56,6 +63,16 @@ if(list1.length == list2.length) {
 
   }
 
+  def limitFib(n: Int) = {
+     def acc(list: List[Int], i: Int): List[Int] = {
+       if (i >= n || n < 2) list
+       else {
+         val newList = list ::: List(list(i-1)+list(i-2))
+         acc(newList, i+1)
+       }
+     }
+     acc(List(0, 1), 2)
+   }
 
 
 }
@@ -69,15 +86,16 @@ object UsingOperations extends App {
   val first = 5
   val second = 4
   val third = 7
+  val num =5
   val obj=new Operations
-  val newList = println("sumOfList"+ obj.sumOfLists(List(one,two,three),List(first,second,third)))
-  val listDouble = println("DoubleElemets" + obj.doubleElements(List(one,two,three)))
+  val newList = print("sumOfList:" + obj.sumOfLists(List(one,two,three),List(first,second,third)) + "\n")
+  val listDouble = print("DoubleElemets:" + obj.doubleElements(List(one,two,three)) + "\n")
   //val shape = rectangle
-
- val positionElement = println("ElementPosition"+ obj.elemPosition(2,(List(one,two,three))))
-  // val areaResult = println((obj.area(shape,first,second,(5,4)))
-  val listLength = println("ListLength"+ obj.listLength(List(one,two,three)))
-  val reverse = println("ListReverse"+ obj.reverseList(List(one,two,three)))
+ val fibonacci = print("fibonacci" + obj.limitFib(num) + "\n")
+ val positionElement = print("ElementPosition:" + obj.elemPosition(2,(List(one,two,three))) + "\n")
+  // val areaResult = print((obj.area(shape,first,second,(5,4)) + "\n")
+  val listLength = print("ListLength:" + obj.listLength(List(one,two,three)) + "\n")
+  val reverse = print("ListReverse:" + obj.reverseList(List(one,two,three)) + "\n")
 
 
 }
